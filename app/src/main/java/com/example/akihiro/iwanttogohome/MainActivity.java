c2a91ee42d0c5d006b35811ddf6e53bbd983ddbf
a1no <a1no2jz2l3@yahoo.co.jp>
2016-11-11 14:58:40 +0900

    'mapアクティビティ作る前　バックアップ'

--- app/src/main/java/com/example/akihiro/iwanttogohome/MainActivity.java
+++ app/src/main/java/com/example/akihiro/iwanttogohome/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.akihiro.iwanttogohome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView main_text;
+    ListView main_ListView;
+    Button mainHome_btn, registration_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_text = (TextView)findViewById(R.id.main_text);
+        main_ListView = (ListView)findViewById(R.id.main_Listview);
+        mainHome_btn = (Button)findViewById(R.id.mainHome_btn);
+        mainHome_btn.setOnClickListener(this);
+        registration_btn = (Button)findViewById(R.id.registration_btn);
+        registration_btn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.mainHome_btn:
+                break;
+            case R.id.registration_btn:
+                break;
+        }
+
     }
 }
